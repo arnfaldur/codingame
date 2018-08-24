6846500d4dd0463f271dc1408604d722b8334fa5
arnfaldur <aarnaldur@gmail.com>
2018-08-24 01:19:08 +0000

    back to you

--- Legends of Code and Magic/Analysis/analyze.jl
+++ Legends of Code and Magic/Analysis/analyze.jl
@@ -7,11 +7,11 @@
       dmg::Int
       hp::Int
       breakthrough::Bool
-      charge::Bool
-      drain::Bool
-      guard::Bool
+      Charge::Bool
+      Drain::Bool
+      Guard::Bool
       Lethal::Bool
-      ward::Bool
+      Ward::Bool
       myhc::Int
       ophc::Int
       draw::Int
@@ -27,23 +27,23 @@ end
 function makeCard(c)
       id = parse(Int,c[1])
       name = c[2]
-      type = c[3] == "creature" ? 0 : c[3] == "itemGreen" ? 1 : c[3] == "itemRed" ? 2 : 3
+      type = c[3] == "Creature" ? 0 : c[3] == "itemGreen" ? 1 : c[3] == "itemRed" ? 2 : 3
       cost = parse(Int, c[4])
       dmg = parse(Int, c[5])
       hp = parse(Int, c[6])
       breakthrough = any(x->x=='B', c[7])
-      charge = any(x->x=='C', c[7])
-      drain = any(x->x=='D', c[7])
-      guard = any(x->x=='G', c[7])
-      lethal = any(x->x=='L', c[7])
-      ward = any(x->x=='W', c[7])
+      Charge = any(x->x=='C', c[7])
+      Drain = any(x->x=='D', c[7])
+      Guard = any(x->x=='G', c[7])
+      Lethal = any(x->x=='L', c[7])
+      Ward = any(x->x=='W', c[7])
       myhc = parse(Int, c[8])
       ophc = parse(Int, c[9])
       draw = parse(Int, c[10])
-      return Card(id,name,type,cost,dmg,hp,breakthrough,charge,drain,guard,lethal,ward,myhc,ophc,draw)
+      return Card(id,name,type,cost,dmg,hp,breakthrough,Charge,Drain,Guard,Lethal,Ward,myhc,ophc,draw)
 end
 
-toInts(ca)=[ca.cost, ca.type == 0, ca.type == 1, ca.type == 2, ca.type == 3, ca.dmg, ca.hp, ca.breakthrough, ca.charge, ca.drain, ca.guard, ca.Lethal, ca.ward, ca.myhc, ca.ophc, ca.draw]
+toInts(ca)=[ca.cost, ca.type == 0, ca.type == 1, ca.type == 2, ca.type == 3, ca.dmg, ca.hp, ca.breakthrough, ca.Charge, ca.Drain, ca.Guard, ca.Lethal, ca.Ward, ca.myhc, ca.ophc, ca.draw]
 
 function getCards()
       return map(makeCard, [split(e, " ; ") for e in split(readCardList(), '\n')][1:end-1])
